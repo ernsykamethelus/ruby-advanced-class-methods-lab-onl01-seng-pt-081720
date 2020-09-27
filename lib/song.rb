39517ae79a074df0eb815bca4e748c9f76b1015e
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 01:43:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,10 @@ class Song
   end
   
   def self.find_or_create_by_name
-    self.find_by_name || self.create_by_name
+    if self.find_by_name(name)
+      sefl.find_by_name(name)
+      else
+        self.create_by_name(name)
   end
   
   def self.alphabetical
