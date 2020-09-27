e275ed1205c54fc8caa7754378659360c863f6b7
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 01:41:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,7 @@ class Song
   end
   
   def self.find_by_name(name) 
-    @@all.each do |n| 
-    puts n.name == name
+    @@all.detect{ |song| song.name == name }
   end
   
   def self.find_or_create_by_name
