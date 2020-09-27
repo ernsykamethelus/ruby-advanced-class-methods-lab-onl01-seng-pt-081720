4682ae516d56576f5d1c9d3657f45d1920793b65
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 01:12:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,10 @@ class Song
     song
   end
   
-  def self.new_by_name
-    
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
   end
   
   def self.create_by_name
